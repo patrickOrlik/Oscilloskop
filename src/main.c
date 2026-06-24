a4ee0f56261c8b5b667bffe1ac4afd16b6a25654
Your Full Name <patrickmathiasorlik@gmail.com>
2026-06-24 10:55:36 +0200

    added comments

--- src/main.c
+++ src/main.c
@@ -28,11 +28,12 @@ unsigned char SelectCounter = 0;
 volatile int Datalength = 0;
 volatile int indexcount = 0;
 volatile bool Adcready = false; // Used for checking if Adc is ready.
-volatile unsigned char Adcvalue;    // Used for storing ADCvalues
 volatile bool Receiveflag = false;
 bool bode = false;
+bool FirstRun = true;
 volatile unsigned char lastChannel = 0;
 volatile bool bodeCollect = false;
+
 unsigned char Protocol = 0;// change this to change UART protocol 
 unsigned char amplitude;
 unsigned char amplitudeRef;
@@ -45,16 +46,15 @@ int dataBcnt = 0;
 // declaration of all data arrays 
 unsigned char ADCdataA[1000];
 unsigned char ADCdataB[1000];
-volatile unsigned char *writeBuffer = ADCdataA;
+volatile unsigned char *writeBuffer = ADCdataA; 
 volatile unsigned char *readBuffer  = ADCdataB;
 unsigned char RXdata[256];
-unsigned char Settings[4] = {0,3,0x7f,0x05};
+unsigned char Settings[4] = {0,0,0,0};
 uint16_t OscSettings[2] = {10000,50}; //stores Osciloscope settings [0]= Sample rate [1]= Packet length
 unsigned char SortingArrayInput[100];
 unsigned char SortingArrayOutput[100];
 unsigned char BodeArray[256];
-unsigned char bodeCh0[100];
-unsigned char bodeCh1[100];
+
 
 
 
@@ -64,7 +64,7 @@ int main()
     DDRB |= (1 << PB7);
     DDRB &= ~(1 << PA0); // sets ADC0 as input (Pin(22))
     DDRH |= (1 << PH4);  // sets pin 7 as output.
-    DDRB |= (PB0);
+    DDRB |= (1 << PB0);
     
 
     sei();
@@ -78,15 +78,18 @@ int main()
     
     while (1)
     {
+        // transmits osciloscope data to labview
         if (Adcready){
        Uart1Transmit(OscSettings[1]+7,0x02,(unsigned char *)readBuffer);
         Adcready = false;
         }
+
     if(Receiveflag){   
+        // checks what type of data has been received
         switch(RXdata[4]){
         case BTN:
              switch(RXdata[5]){
-                case BTN0:
+                case BTN0: // enter button
                 Settings[SelectCounter+1]= RXdata[6];
                 Uart1Transmit(12,1,Settings);
                 SPI_FpgaTransmit((SelectCounter),RXdata[6]);
@@ -94,7 +97,7 @@ int main()
 
                 break;
                 
-                case BTN1:
+                case BTN1: // select button
                 SelectCounter++;
                 Settings[0]=SelectCounter;
                 Uart1Transmit(12,1,Settings);
@@ -104,13 +107,13 @@ int main()
                 Receiveflag = false;
 
                 break;
-                case BTN2:
+                case BTN2: // run/stop button
                 SPI_FpgaTransmit(0x03,0x00);
                 Receiveflag = false;
 
                 break;
 
-                case BTN3:
+                case BTN3: // reset
                 SPI_FpgaTransmit(0x04,0x00);
                 memset(Settings,0,4);
                 Uart1Transmit(12,1,Settings);
@@ -132,8 +135,9 @@ int main()
         case SEND:
         OscSettings[Srate] = (RXdata[5]<< 8) | RXdata[6];
         OscSettings[Rlen] =(RXdata[7]<< 8) | RXdata[8];
+        
         long MinRlen = (7L * OscSettings[Srate]) / (11520L - OscSettings[Srate]);
-        if (OscSettings[Rlen] < MinRlen){
+        if (OscSettings[Rlen] < MinRlen){  //Checks Rlen below min
             OscSettings[Rlen] = (uint16_t)MinRlen;
             char buff[50];
             sprintf(buff,"Too low Rlen set. Rlen set to: %d",OscSettings[Rlen]);
@@ -153,12 +157,9 @@ int main()
         SPI_FpgaTransmit(run,0x00);
 
         for(int i = 1; i <= 255; i++){
-            dataAcnt = 0;
-           dataBcnt = 0;
-            channel = 0;
-            lastChannel = 0;
             bodeCollect = false;
-           uint32_t fpga_freq = 24UL + (uint32_t)(i - 1) * 23676UL / 254UL;
+            //calculates appropiate sample rate
+           uint32_t fpga_freq  = 24UL + (uint32_t)(i - 1) * 93UL;
             uint32_t sampleRate = fpga_freq * 20;
             if (sampleRate > 40000) sampleRate = 40000;
             if (sampleRate < 2000)  sampleRate = 2000;
@@ -169,21 +170,19 @@ int main()
             cli();
             memcpy(SortingArrayInput,ADCdataA,100);
             memcpy(SortingArrayOutput,ADCdataB,100);
+            // sets the reference amplitude in the first run
             if(i == 1){
+                FirstRun = true;
                  qsort(SortingArrayInput,100,sizeof(unsigned char),comp);
                  amplitudeRef = (SortingArrayInput[99]-SortingArrayInput[0]);
                  BodeArray[i] = amplitudeRef;
-                 char buff[50];
-            sprintf(buff,"ref amplitude is %d \n",amplitudeRef);
-            putstringuart0(buff);
+          
             }
             qsort (SortingArrayOutput,100,sizeof(unsigned char),comp);
              amplitude = (SortingArrayOutput[99]-SortingArrayOutput[0]);
             
              BodeArray[i] = (unsigned char)((255UL * amplitude) / amplitudeRef);
-             char buff[50];
-             sprintf(buff,"dampening is %d %d",amplitude,BodeArray[i]);
-            putstringuart0(buff);
+          
         
              sei();
         }
@@ -210,39 +209,41 @@ int main()
 ISR(ADC_vect)
 {
     if (bode) {
-        
-        if (lastChannel == 0 && dataAcnt < 100) {
-            ADCdataA[dataAcnt++] = ADCH;
-        } else if (lastChannel == 1 && dataBcnt < 100) {
-            ADCdataB[dataBcnt++] = ADCH;
-        }
-        if (dataBcnt >= 100 && dataAcnt >= 100) {
-            dataAcnt = 0;
-            dataBcnt = 0;
-            bodeCollect = true;
-            
+        //checks if this is the first time bode is running
+        if (FirstRun){
+            ADCdataA[indexcount++]=ADCH;}
+            else {
+            ADCdataB[indexcount++]=ADCH;
+            }
+              if(indexcount >= 100){
+                FirstRun = false;
+                indexcount =0;
+                bodeCollect = true;
+               
+    }
+
+    } else {
+        writeBuffer[indexcount++] = ADCH;
+        if(indexcount >= OscSettings[Rlen]){
+            volatile unsigned char *tmp = readBuffer;
+            readBuffer = writeBuffer;
+            writeBuffer = tmp;
+            indexcount = 0;
+            Adcready = true;
         }
     }
-    else{
-    writeBuffer[indexcount] = ADCH;
-    indexcount++;
-    if(indexcount >= OscSettings[Rlen]){
-        volatile unsigned char *tmp = readBuffer;
-        readBuffer = writeBuffer;
-        writeBuffer = tmp;
-        indexcount = 0;
-        Adcready = true;
-    }
-}
 }
 
 ISR(TIMER1_COMPA_vect)
 {
+    //checks if bodeplot function is active
     if (bode) {
-        lastChannel = channel;
-        select_channel(channel);
-        channel++;
-        if (channel == 2) { channel = 0; }
+        if(FirstRun){
+            select_channel(0); 
+        }
+        else {
+            select_channel(1);
+        }
     } else {
         select_channel(0);
     }
@@ -254,7 +255,7 @@ ISR(USART1_RX_vect)
     Data = UDR1;
     
     PORTB ^= (1<< PB7);
-           if (Receiveflag) return;// sikrer at vi når at kører alt koden i main når vi har modtaget en pakke
+           if (Receiveflag) return;//secures that we havde handled previous received packet before handling a new one
           RXdata[count] = Data;
              count++;
              if (count == 3)
@@ -274,7 +275,7 @@ ISR(USART1_RX_vect)
             }
             else if (count == Datalength)
             {
-                if (Protocol){
+                if (Protocol){//checks which protocol is selected
                         Chksum = 0;
                    for (int i =0; i <(Datalength); i++){
                   Chksum = Chksum ^ RXdata[i];
@@ -306,6 +307,7 @@ ISR(USART1_RX_vect)
    
 }
 }
+//comp function used to sort in the bodeplot
 int comp(const void *a, const void *b) {
     return (*(const unsigned char *)a - *(const unsigned char *)b);
 }
